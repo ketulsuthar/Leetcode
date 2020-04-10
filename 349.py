945ef4faad0da17cbed35129460bd3f317e10aff
ketulsuthar <k2lsuthar@gmail.com>
2020-04-09 22:25:53 -0400

    Solution for Intersection of Two Arrays

---
+++ 349.py
@@ -0,0 +1,64 @@
+'''
+349. Intersection of Two Arrays
+
+Given two arrays, write a function to compute their intersection.
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Note:
+
+Each element in the result must be unique.
+The result can be in any order.
+
+'''
+
+# Solution-1
+
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        a = set([i for i in nums1 if i in nums2])
+        return a
+
+
+
+# Solution-2
+
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        
+        return set(nums1) & set(nums2)
+
+
+
+# Solution-3
+
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        nums1 = set(nums1)
+        nums2 = set(nums2)
+        
+        if len(nums1) > len(nums2):
+            return [i for i in nums2 if i in nums1]
+        else:
+             return [i for i in nums1 if i in nums2]
