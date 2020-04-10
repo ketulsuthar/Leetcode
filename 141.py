c84bd23161366796ce995c098f190ad962246db7
ketulsuthar <k2lsuthar@gmail.com>
2020-04-09 23:29:46 -0400

    Solution for Linked List Cycle

---
+++ 141.py
@@ -0,0 +1,87 @@
+'''
+141.  Linked List Cycle
+
+Given a linked list, determine if it has a cycle in it.
+
+To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+
+ 
+
+Example 1:
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+
+Example 2:
+
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where tail connects to the first node.
+
+
+Example 3:
+
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+
+
+ 
+
+Follow up:
+
+Can you solve it using O(1) (i.e. constant) memory?
+
+'''
+
+
+# Solution -1
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if head == None: return False
+        fast = head.next
+        slow = head
+        while fast != slow:
+            if fast == None or fast.next == None:
+                return False
+            
+            fast = fast.next.next
+            slow=slow.next
+        return True
+
+
+# Solution -2
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        linked = {}
+        while head != None:
+            if head in linked:
+                return True
+            linked[head]=head
+            head = head.next
+            
+        return False
