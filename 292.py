5c62e7d9e53536c7523fa2cd2517cc097223cb0a
k2lsuthar@gmail.com <k2lsuthar@gmail.com>
2020-04-12 03:13:34 -0400

    Add solution and Reasoning

--- 292.py
+++ 292.py
@@ -12,4 +12,22 @@ Output: false
 Explanation: If there are 4 stones in the heap, then you will never win the game;
              No matter 1, 2, or 3 stones you remove, the last stone will always be 
              removed by your friend.
+
+
+Reasoning:
+
+Let us think of the small cases. It is clear that if there are only one, two, or three stones in the pile, and it is your turn, you can win the game by taking all of them. Like the problem description says, if there are exactly four stones in the pile, you will lose. Because no matter how many you take, you will leave some stones behind for your opponent to take and win the game. So in order to win, you have to ensure that you never reach the situation where there are exactly four stones on the pile on your turn.
+
+Similarly, if there are five, six, or seven stones you can win by taking just enough to leave four stones for your opponent so that they lose. But if there are eight stones on the pile, you will inevitably lose, because regardless whether you pick one, two or three stones from the pile, your opponent can pick three, two or one stone to ensure that, again, four stones will be left to you on your turn.
+
+It is obvious that the same pattern repeats itself for n=4,8,12,16,\dotsn=4,8,12,16,…, basically all multiples of 44.
 '''
+
+
+class Solution(object):
+    def canWinNim(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return not n%4 == 0
